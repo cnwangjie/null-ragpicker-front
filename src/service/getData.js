605a7266def7962ee3c51b83c797ad1fade93e46
Wang Jie <doudou19758@gmail.com>
2018-03-05 17:16:57 +0800

    feat: add service without tests

---
+++ src/service/getData.js
@@ -0,0 +1,96 @@
+import {apiUrl} from './config'
+
+const tokenKey = 'jwt-token'
+const tokenHeader = 'Authorization'
+
+const getCookie = key => {
+  const cookie = document.cookie
+    .split(';')
+    .map(i => i.trim().split('='))
+    .filter(i => i.shift() === key)
+
+  if (cookie.length === 0) return null
+  return cookie.toString()
+}
+
+const getToken = () => {
+  const cookie = getCookie(tokenKey)
+  if (cookie) {
+    localStorage.setItem(tokenKey, cookie)
+    return cookie
+  }
+  return localStorage.getItem(tokenKey)
+}
+
+const fetchData = (uri = '', method = 'GET', data = {}) => {
+  const headers = new Headers
+  const token = getToken()
+  if (token) headers.append(tokenHeader, token)
+
+  const option = {
+    headers,
+    method,
+    mode: 'cors',
+  }
+
+  let requestBody = Object.keys(data).map(key => {
+    return key + '=' + encodeURIComponent(data[key])
+  }).filter(i => i).join('&')
+
+  if (method === 'POST') {
+    headers.append('Content-Type', 'application/x-www-form-urlencoded')
+    option.body = requestBody
+  } else {
+    uri += '?' + requestBody
+  }
+
+  return fetch(apiUrl + uri, option)
+    .then(data => data.json())
+    .then(json => {
+      if ('error' in json && json.error === 'Unauthorized') {
+        localStorage.removeItem(tokenKey)
+        location.href = location.href
+      }
+      return json
+    })
+    .catch(err => {
+      return {
+        status: 'error',
+        error: 'response_error',
+        msg: 'Internal Server Error',
+      }
+    })
+}
+
+export const listUserAddress = userId => fetchData(`/api/user/${userId}/address`)
+
+export const addAddress = (userId, address) => fetchData(`/api/user/${userId}/address`, 'POST', address)
+
+export const getAddress = addressId => fetchData(`/api/address/${addressId}`)
+
+export const updateAddress = (userId, address) => fetchData(`/api/address/${addressId}`, 'POST', address)
+
+export const deleteAddress = addressId => fetch(`/api/address/${addressId}/delete`, 'POST')
+
+export const listAllCates = () => fetch(`/api/cate`)
+
+export const getUserInfo = userId => fetchData(`/api/${userId}/info`)
+
+export const listOrdersByUser = userId => fetchData(`/api/user/${userId}/order`)
+
+export const createOrder = (userId, {addressId, remark, orderDetails}) => fetchData(`/api/user/${userId}/order`, 'POST', {
+  address_id,
+  remark: remark || '',
+  order_details: JSON.stringify(orderDetails),
+})
+
+export const deleteOrder = orderNo => fetchData(`/api/order/${orderNo}/cancel`, 'POST')
+
+export const listOrdersByCollector = collectorId => fetchData(`/api/order/${collectorId}/order`)
+
+export const completeOrder = (orderNo, amount, orderDetails) => fetchData(`/api/order/${orderNo}/complete`, 'POST', {
+  amount,
+  order_details: JSON.stringify(orderDetails),
+})
+
+export const getOrderDetail = orderNo => fetchData(`/api/order/${orderNo}`)
